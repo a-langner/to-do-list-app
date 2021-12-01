960fad8618c60647a7ccbc9cc994c91dc23083a1
a-langner <andreas_langner@yahoo.de>
2021-12-01 18:21:13 +0100

    check-box

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,12 +1,18 @@
+import "../SCSS/CheckBox.scss";
+import React, { useState } from 'react';
+
 const CheckBox = () => {
+    const [checkBox, setCheckBox] = useState(false);
+    const switchCheckBox = () => setCheckBox(!checkBox);
+
     return (
         <div>
-            <label class="CheckBox">
-                <input type="checkbox" />
+            <label class="CheckBox" >
+                <input type="checkbox" id="checkBox" onClick={switchCheckBox} />
                 <span class="FakeInput"></span>
                 {/* <span>Finished</span> */}
             </label>
-            <div>Finished</div>
+            <label for="checkBox" class="checkBoxLabel">{checkBox ? "Finished" : "Unfinished"}</label>
         </div>
     );
 };
